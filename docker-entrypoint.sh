218f2b28076b04b521c234cd7be4587aa82566d7
Johannes Westphal <johannes.westphal@posteo.de>
2021-05-15 13:03:48 +0200

    Add missing shebang

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
-# On container startup log in with username and password if given and start the server
-if [ -n ${USERNAME} -a -n ${PASSWORD} ]; then
+#!/bin/bash
+set -e
+
+# Log in with username and password if given
+if [ -n ${USERNAME} -a -n ${PASSWORD} ]
+then
     supertuxkart --init-user --login=${USERNAME} --password=${PASSWORD}
-fi &&
+fi
+
+# Start the server
 supertuxkart --server-config=server_config.xml
